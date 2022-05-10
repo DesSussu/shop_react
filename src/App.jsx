3dbd897e34fd0e36a8f765648233f88dbbe186c0
Desire Maya <desiremrv@gmail.com>
2022-05-10 17:34:44 +0200

    prueba contador

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Button from "./components/Button/Button";
 import logo from "./assets/img/logo.png";
 import Navbar from "./layout/NavBar/navbar.jsx";
+import Add from "./components/Button/add";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Navbar />
         <img src={logo} className="logo" />
         <div>
-          <Button title="SHOP NOW" color="glitch" />
+          <Add />
         </div>
       </header>
     </div>
---
+++ src/components/Button/add.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export default function Add() {
+  const [count, setCount] = useState(0);
+
+  const sumar = () => {
+    setCount(count + 1);
+  };
+
+  const restar = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      alert("no puedes quitar más");
+    }
+  };
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={sumar}>Sumar</button>
+      <button onClick={restar}>Restar</button>
+    </div>
+  );
+}
