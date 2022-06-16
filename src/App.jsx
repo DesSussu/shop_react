5e346000dbf1957283df7afd291e33f42bcca3df
Desire Maya <desiremrv@gmail.com>
2022-06-16 19:48:48 +0200

    final commit cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Home from "../src/pages/Home";
-import Navbar from "../src/layout/NavBar/Navbar";
+import Navbar from "./layout/NavBar/Navbar";
 import ItemListContainer from "../src/layout/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "../src/layout/itemDetailContainer/itemDetailContainer";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
--- src/layout/NavBar/navbar.jsx
+++ src/layout/NavBar/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import home from "../../assets/img/home.svg";
 import { Link } from "react-router-dom";
-import CartWidget from "../../../src/components/CartWidget/CartWidget";
+import CartWidget from "../../components/CartWidget/CartWidget";
 import "../NavBar/Navbar.css";
 
 function Navbar() {
